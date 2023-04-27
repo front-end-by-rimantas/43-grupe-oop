fd31264a92d1b07127a900f0e2d9ab04afd79345
Rimantas <belauzas@yahoo.com>
2023-04-27 14:47:16 +0300

    School update

--- index.js
+++ index.js
@@ -12,13 +12,27 @@ console.log(zuikiu);
 
 console.log(zuikiu.description());
 
+zuikiu.addMark('Petras', 10);
+zuikiu.addMark('Petras', 2);
+zuikiu.addMark('Maryte', 2);
+zuikiu.addMark('Maryte', 8);
+zuikiu.addMark('Jonas', 8);
+zuikiu.addMark('Jonas', 7);
+zuikiu.addMark('Jonas', 6);
+zuikiu.addMark('Ona', 4);
+zuikiu.addMark('Ona', 6);
+zuikiu.addMark('Ona', 8);
+zuikiu.addMark('Ona', 10);
 
+console.log(zuikiu.students);
 
+console.log(zuikiu.average('Petras'));
+console.log(zuikiu.average('Maryte'));
+console.log(zuikiu.average('Jonas'));
+console.log(zuikiu.average('Ona'));
 
+zuikiu.addStudent('Abrahimas');
+// zuikiu.addMark('Abrahimas', 9);
+console.log(zuikiu.average('Abrahimas'));
 
-
-
-
-// const fruktu = new School('Fruktu');
-// console.log(fruktu);
-// console.log(fruktu.name, fruktu.kids);
+console.log(zuikiu.average('Al Bundy'));
